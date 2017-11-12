86bdf9474939bc2642dea1805d7595fe5ed03790
Covina <jabs303@gmail.com>
2017-11-12 13:28:04 -0800

    Section 5, Lecture 21:  Cube movement and multiple cameras

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,36 +4,80 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+
+    public GameObject[] gameCameras;
+
+    // Track which camera we want to look at.
+    private int gameCameraIndex = 0;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        // set main camera
+        FocusOnCamera(gameCameraIndex);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetMouseButtonDown(0))
         {
-
-            Debug.Log("Jump");
-
+            // change direction
+            ChangeCamera(1);
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetMouseButtonDown(1))
         {
-
-            Debug.Log("Move Left");
-
+            // change direction
+            ChangeCamera(-1);
         }
 
+    }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+
+    private void FocusOnCamera(int index)
+    {
+        for(int i = 0; i < gameCameras.Length; i++)
         {
 
-            Debug.Log("Move Right");
+            if (i == index)
+            {
+                gameCameras[i].SetActive(true);
+                continue;
+
+            }
+
+            gameCameras[i].SetActive(false);
+
 
         }
 
 
     }
+
+    /// <summary>
+    /// Change Camera
+    /// </summary>
+    /// <param name="changeDirection"></param>
+    private void ChangeCamera(int changeDirection)
+    {
+        // move one index
+        gameCameraIndex += changeDirection;
+
+        if(gameCameraIndex >= gameCameras.Length)
+        {
+            // out of range, wrap to first one
+            gameCameraIndex = 0;
+
+        } else if (gameCameraIndex < 0)
+        {
+            // out of range, wrap around to last one
+            gameCameraIndex = gameCameras.Length - 1;
+        }
+
+        FocusOnCamera(gameCameraIndex);
+
+
+    }
+
 }
