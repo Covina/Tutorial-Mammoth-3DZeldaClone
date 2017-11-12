fbeec2f96389e72034ce92133dfd1167c5689d4a
Covina <jabs303@gmail.com>
2017-11-12 10:24:14 -0800

    Section 4, Lecture 17 - Basic input tests

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Debug.Log("Jump");
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            Debug.Log("Move Left");
+
+        }
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            Debug.Log("Move Right");
+
+        }
+
+
+    }
+}
